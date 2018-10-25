89d1c4f210ccccdf270391b304d18229cf464ae7
github username <github email address>
2018-10-25 10:44:44 +0100

    Add summary

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const yargs = require('yargs')
-const axios = require('axios')
+const yargs = require('yargs');
+const axios = require('axios');
 
 
 const argv = yargs.options({
@@ -19,31 +19,32 @@ const argv = yargs.options({
 .alias('help', 'h')
 .argv;
 
-let encodedAddress = (argv.address)
-let geocodeUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=vkDHXzlLG0er9G0mAKXCk1UPN4213zwA&location=${encodedAddress}`
+let encodedAddress = (argv.address);
+let geocodeUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=vkDHXzlLG0er9G0mAKXCk1UPN4213zwA&location=${encodedAddress}`;
 
 axios.get(geocodeUrl).then((response) =>{
 	if (response.data.info.statuscode === 400){
-		throw new Error('Unable to find that address')
+		throw new Error('Unable to find that address');
 	}
 
-	let latitude = response.data.results[0].locations[0].latLng.lat
-	let longitude = response.data.results[0].locations[0].latLng.lng
-	weatherUrl = `https://api.darksky.net/forecast/31d62bebec32e0206831a5a1dc9bfcd7/${latitude},${longitude}`
-	console.log(response.data.results[0].locations[0].adminArea5)
-	return axios.get(weatherUrl)
+	let latitude = response.data.results[0].locations[0].latLng.lat;
+	let longitude = response.data.results[0].locations[0].latLng.lng;
+	weatherUrl = `https://api.darksky.net/forecast/31d62bebec32e0206831a5a1dc9bfcd7/${latitude},${longitude}`;
+	console.log(response.data.results[0].locations[0].adminArea5);
+	return axios.get(weatherUrl);
 }).then((response) =>{
-	let temperature = Math.floor((response.data.currently.temperature - 32) * 5/9)
-	let apparentTemperature = Math.floor((response.data.currently.apparentTemperature -32) *5/9)
-	console.log(`It's currently ${temperature}˚C. It feels like ${apparentTemperature}˚C.`)
+	let temperature = Math.floor((response.data.currently.temperature - 32) * 5/9);
+	let apparentTemperature = Math.floor((response.data.currently.apparentTemperature -32) *5/9);
+	let summary = response.data.currently.summary;
+	console.log(`It's ${summary}, temperature of ${temperature}˚C. It feels like ${apparentTemperature}˚C.`);
 }).catch((e) =>{
 
 	if (e.code === 'ENOTFOUND'){
-		console.log('Unable to connect to API server')
+		console.log('Unable to connect to API server');
 	}else{
-		console.log(e.message)
+		console.log(e.message);
 	}
-})
+});
 
 
 
